e18dfab1172415197116f0956f9dbffa8a3d55e8
Cheikhoul Khadim SECK <58985890+Cheikhoul@users.noreply.github.com>
2022-02-20 23:08:03 +0100

    Inclusion de formURL dans la session en cours

--- pages/formUrl.php
+++ pages/formUrl.php
@@ -1,17 +1,23 @@
-<?php require_once('../assets/header.php'); ?>
+<?php 
+require('../login/config.php');
+require_once('../assets/header.php'); ?>
 <h1>URL</h1>
 <?php require_once('../function/shortenUrl.php') ?>
-<?php require_once('../function/dataLinker.php') ?>
+<?php require_once('../function/template.dataLinker.php') ?>
+
 <?php
-if ($_POST['url']) {
-  $DB = DatabaseLinker::getConnexion();
-
-  try {
-    saveUrl($DB);
-  } catch (\Throwable $th) {
-    throw $th;
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION["pseudo"])){
+    header("Location: login.php");
+    exit(); 
   }
+?>
 
+<?php
+
+if ($_POST['url']) {
   echo "
     <form action='' method='POST'>
       <label for='url'>
@@ -45,7 +51,7 @@ if ($_POST['url']) {
     <?php
     // TODO Add variable for id_utilisateur
 
-    $getGeneratedURL = $DB->prepare("SELECT url_origine.url_origine, url_raccourcie.url_raccourcie FROM utilisateur 
+    $getGeneratedURL = $conn->prepare("SELECT url_origine.url_origine, url_raccourcie.url_raccourcie FROM utilisateur 
     JOIN url_origine ON utilisateur.id_utilisateur = url_origine.id_utilisateur
     JOIN url_raccourcie ON url_origine.id_url_origine = url_raccourcie.id_url_origine
     WHERE utilisateur.id_utilisateur = 1");
